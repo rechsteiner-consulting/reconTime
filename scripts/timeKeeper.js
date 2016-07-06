2921ceb405a56854f3a049ed065f2d8ffbe1a64f
4FiFFi <p.weber@recon.ch>
2016-07-06 15:42:29 +0200

    refactored
    add methode

--- scripts/timeKeeper.js
+++ scripts/timeKeeper.js
@@ -7,44 +7,58 @@ window.onload = function()
 }
 
 function time() {
-    var now = new Date();
-    var hours = now.getHours();
-    var minutes = now.getMinutes();
-    var seconds = now.getSeconds();
- 	var time = "";
-	
-    time = (hours < 10) ? "0" + hours + ":" : hours + ":";
-    time += (minutes < 10) ? "0" + minutes + ":" : minutes + ":";
-    time += (seconds < 10) ? "0" + seconds : seconds;
- 
-    document.getElementById('time').innerHTML = '<h1>' + time + '</h1>';
+    var now = new Date();     
+    document.getElementById('time').innerHTML = '<h1>' + timeOutputString(now) + '</h1>';
 }
 
 function startWork() {
-	var now = new Date();
-	var startWorkHours = now.getHours();
-	var startWorkMinutes = now.getMinutes();
-    var startWorkSeconds = now.getSeconds();
-	var startTime = "";
-	
-	startTime = (startWorkHours < 10) ? "0" + startWorkHours + ":" : startWorkHours + ":";
-    startTime += (startWorkMinutes < 10) ? "0" + startWorkMinutes + ":" : startWorkMinutes + ":";
-    startTime += (startWorkSeconds < 10) ? "0" + startWorkSeconds : startWorkSeconds;
-	
-	document.getElementById('modalStartBody').innerHTML =  startTime ;
-	
+	var now = new Date();		
+	document.getElementById('modalStartBody').innerHTML =  timeOutputString(now);	
 }
 
 function endWork() {
-	var now = new Date();
-	var endWorkHours = now.getHours();
-	var endWorkMinutes = now.getMinutes();
-    var endWorkSeconds = now.getSeconds();
-	var endTime = "";
+	var now = new Date();	
+	document.getElementById('modalEndBody').innerHTML =  timeOutputString(now);
+}
+
+function timeOutputString(date) {	
+	var hours = date.getHours();
+	var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+	var time = "";
+	time = (hours < 10) ? "0" + hours + ":" : hours + ":";
+    time += (minutes < 10) ? "0" + minutes + ":" : minutes + ":";
+    time += (seconds < 10) ? "0" + seconds : seconds;
+	return time;
+}
+
+function elapsedTime(){
+	//values set to millisecond
+	var msecPerMinute = 1000 * 60;
+	var msecPerHour = msecPerMinute * 60;
+	var msecPerDay = msecPerHour * 24;
 	
-	endTime = (endWorkHours < 10) ? "0" + endWorkHours + ":" : endWorkHours + ":";
-    endTime += (endWorkMinutes < 10) ? "0" + endWorkMinutes + ":" : endWorkMinutes + ":";
-    endTime += (endWorkSeconds < 10) ? "0" + endWorkSeconds : endWorkSeconds;
+	//Set the Date
+	var date = new Date();
+	var dateMsec = date.getTime();
 	
-	document.getElementById('modalEndBody').innerHTML =  endTime ;
+	date.getMonth();
+	date.getDate();
+	date.getHours();
+	
+	//get the diffrence in milliseconds.
+	var interval = dateMsec - date.getTime();
+	
+	//calculate how many days
+	var days = Math.floor(interval / msecPerDay);
+	interval = interval - (days * msecPerDay);
+	
+	//calculate hours, minutes and seconds
+	var hours = Math.floor(interval / msecPerHour);
+	interval = interval - (hours * msecPerHour);
+	
+	var minutes = Math.floor(interval/msecPerMinute);
+	interval = interval - (minutes * msecPerMinute);
+	
+	var seconds = Math.floor(interval/1000);
 }

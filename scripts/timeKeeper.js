1b2e5dd77ad9bd4301057b7ae6566aec8c839070
4FiFFi <p.weber@recon.ch>
2016-07-06 11:12:40 +0200

    startWork, endWork function added
    -Zwei Funktionen erstellt um die start und endzeit zu ermitteln

--- scripts/timeKeeper.js
+++ scripts/timeKeeper.js
@@ -18,6 +18,30 @@ function time() {
     document.getElementById('time').innerHTML = '<h1>' + time + '</h1>';
 }
 
-function startTime() {
+function startWork() {
+	var now = new Date();
+	var startWorkHours = now.getHours();
+	var startWorkMinutes = now.getMinutes();
+    var startWorkSeconds = now.getSeconds();
+	var startTime = "";
 	
+	startTime = (startWorkHours < 10) ? "0" + startWorkHours + ":" : startWorkHours + ":";
+    startTime += (startWorkMinutes < 10) ? "0" + startWorkMinutes + ":" : startWorkMinutes + ":";
+    startTime += (startWorkSeconds < 10) ? "0" + startWorkSeconds : startWorkSeconds;
+	
+	document.getElementById('modalStartBody').innerHTML =  startTime ;
+}
+
+function endWork() {
+	var now = new Date();
+	var endWorkHours = now.getHours();
+	var endWorkMinutes = now.getMinutes();
+    var endWorkSeconds = now.getSeconds();
+	var endTime = "";
+	
+	endTime = (endWorkHours < 10) ? "0" + endWorkHours + ":" : endWorkHours + ":";
+    endTime += (endWorkMinutes < 10) ? "0" + endWorkMinutes + ":" : endWorkMinutes + ":";
+    endTime += (endWorkSeconds < 10) ? "0" + endWorkSeconds : endWorkSeconds;
+	
+	document.getElementById('modalEndBody').innerHTML =  endTime ;
 }

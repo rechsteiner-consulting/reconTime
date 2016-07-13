61ce0fec460bc1939862d1af68f785dec437007a
4FiFFi <p.weber@recon.ch>
2016-07-13 10:28:50 +0200

    barcodes and diffTime
    -erste Barcodes erstellt
    -diffTime probiert zu lösen (Läuft noch nicht)

--- scripts/timeKeeper.js
+++ scripts/timeKeeper.js
@@ -3,7 +3,6 @@ window.onload = function()
 	document.getElementById("startButton").addEventListener("click", startWork);
 	document.getElementById("endButton").addEventListener("click", endWork);
     window.setInterval("time()", 1000);
-	
 }
 
 function time() {
@@ -12,53 +11,41 @@ function time() {
 }
 
 function startWork() {
-	var now = new Date();		
-	document.getElementById('modalStartBody').innerHTML =  timeOutputString(now);	
+	startTime = new Date();		
+	document.getElementById('modalStartBody').innerHTML =  timeOutputString(startTime);
+	startTime = Date(timeOutputString(startTime));	
 }
 
 function endWork() {
 	var now = new Date();	
-	document.getElementById('modalEndBody').innerHTML =  timeOutputString(now);
+	var endTime = new Date(timeOutputString(now));// HIER LIEGT DER FEHLER, FINDE IHN! LG JOEL
+	timeOutputString(timeDifference(startTime, endTime)) ;
+	//document.getElementById('modalEndBody').innerHTML =  timeOutputString(timeDifference(startTime, endTime));
 }
 
-function timeOutputString(date) {	
+function timeOutputString(date) {
+	var date = new Date(date);
 	var hours = date.getHours();
 	var minutes = date.getMinutes();
     var seconds = date.getSeconds();
 	var time = "";
+
 	time = (hours < 10) ? "0" + hours + ":" : hours + ":";
     time += (minutes < 10) ? "0" + minutes + ":" : minutes + ":";
     time += (seconds < 10) ? "0" + seconds : seconds;
 	return time;
 }
 
-function elapsedTime(){
-	//values set to millisecond
-	var msecPerMinute = 1000 * 60;
-	var msecPerHour = msecPerMinute * 60;
-	var msecPerDay = msecPerHour * 24;
-	
-	//Set the Date
-	var date = new Date();
-	var dateMsec = date.getTime();
-	
-	date.getMonth();
-	date.getDate();
-	date.getHours();
-	
-	//get the diffrence in milliseconds.
-	var interval = dateMsec - date.getTime();
-	
-	//calculate how many days
-	var days = Math.floor(interval / msecPerDay);
-	interval = interval - (days * msecPerDay);
-	
-	//calculate hours, minutes and seconds
-	var hours = Math.floor(interval / msecPerHour);
-	interval = interval - (hours * msecPerHour);
-	
-	var minutes = Math.floor(interval/msecPerMinute);
-	interval = interval - (minutes * msecPerMinute);
-	
-	var seconds = Math.floor(interval/1000);
+function timeDifference(startTime, endTime) {
+    var endTime = new Date(endTime);
+	var startTime = new Date(startTime);
+	var difference = new Date();
+	difference.setDate(startTime.getDate()-endTime.getDate());
+	difference.setDate(startTime.getSeconds()-endTime.getSeconds());
+  console.log(endTime.getDate());
+  console.log(difference.getDate(), difference.getTime());
+	document.getElementById('modalEndBody').innerHTML =  (endTime.getTime() - startTime.getTime());
+	return difference;
 }
+
+

5ce1ac5fed34bfea9db987ed3df5f5f318febb83
ssnonn <tu@recon.ch>
2016-07-13 08:15:06 +0200

    Admin Panel
    -Admin Panel
    Tabs hinzugeüfgt
    Modal für Benutzer erstellen & entfernen
    
    -Index
    Einlogscript in JS Datei verschoben und verlinkt

---
+++ scripts/loginmodal.js
@@ -0,0 +1,10 @@
+function check(form) { /*function to check userid & password*/
+						/*the following code checkes whether the entered userid and password are matching*/
+						
+						if(form.userid.value == ("") && form.pswrd.value == "") {
+							window.location = "adminpanel.html";/*opens the target page while Id & password matches*/
+						}
+						else {
+							alert("Error Password or Username incorrect")/*displays error message*/
+						}
+}

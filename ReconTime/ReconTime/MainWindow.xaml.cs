cc0d7e7911b453810d727e6b78c943cd12c0117b
simmljoe <simmljoe@students.zhaw.ch>
2016-11-28 20:02:58 +0100

    Simple LogIn / LogOut check

--- ReconTime/ReconTime/MainWindow.xaml.cs
+++ ReconTime/ReconTime/MainWindow.xaml.cs
@@ -24,5 +24,63 @@ namespace ReconTime
         {
             InitializeComponent();
         }
+
+        private void inputBarcode_TextChanged(object sender, TextChangedEventArgs e)
+        {
+           
+        }
+
+        private void inputBarcode_Enter(object sender, DragEventArgs e)
+        {
+            inputValidation();
+        }
+
+        private void checkIn(int barcode)
+        {   //TODO
+            Console.WriteLine("Login: " + barcode);
+        }
+        private void checkOut(int barcode)
+        {   //TODO
+            Console.WriteLine("Logout: " + barcode);
+        }
+        private void printWrongFormat(int length)
+        {
+
+            Console.WriteLine("Falsches Format. Bitte geben Sie eine Nummer mit " + length + " Zeichen ein.");
+        }
+
+        private void inputValidation()
+        {
+            int inputLength = 4;
+            int barcode = 0;
+            String input = inputBarcode.Text;
+            if (input.Length == inputLength)
+            {
+                try
+                {
+                    barcode = int.Parse(input);
+                }
+                catch
+                {
+                    printWrongFormat(inputLength);
+                }
+                if (barcode % 2 == 0)
+                {
+                    checkIn(barcode);
+                }
+                else
+                {
+                    checkOut(barcode);
+                }
+            }
+            else
+            {
+                printWrongFormat(inputLength);
+            }
+        }
+        private void buttonOk_Click(object sender, RoutedEventArgs e)
+        {
+            inputValidation();
+        }
     }
 }
